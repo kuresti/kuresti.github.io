604672b7a47f89f871b3a91c48af560958492751
kuresti <ure17002@byui.edu>
2019-10-15 17:24:31 -0700

    Preston

--- lesson-5/js/lastModified.js
+++
@@ -1,7 +0,0 @@
-var d = new Date();
-document.getElementById("todaysDate").innerHTML = d.toDateString();
-
-//var d = new Date();
-//var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-//document.getElementById("todaysDate").innerHTML = (days[d.getDay()] + "," + d.getDate() + " " + month[d.getMonth()] + " " + d.getFullYear());
