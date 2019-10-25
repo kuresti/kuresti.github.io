d4b538543a0e986ff1d07e28ff1fdef630cffe18
kuresti <ure17002@byui.edu>
2019-10-25 11:49:41 -0700

    preston

---
+++ lesson-6/js/windchill.js
@@ -0,0 +1,17 @@
+
+   var t = parseFloat(document.getElementById("t").textContent);
+   var s = parseFloat(document.getElementById("s").textContent);
+   var f = (t, s);
+
+     f = (35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));
+
+     document.getElementById('chill').textContent = f;
+
+  if (f <= 50 && s > 3) {
+      document.getElementById('chill').textContent = f.toFixed(0) ="\xB0F";
+  }
+  else {
+      document.getElementById('chill').textContent = "NA";
+  }
+
+
